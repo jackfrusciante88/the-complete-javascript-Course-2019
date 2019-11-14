bcf21f6fcfc3d9823845d255a7f8be11b5ed288d
Mattia Carducci <carducci@google.com>
2019-11-14 16:50:22 +0100

    ok

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -34,7 +34,7 @@ driverLicense6(true);
 
 ///////////////////////
 /// block and IFE
-
+/*
 
 //es6
 {
@@ -89,3 +89,51 @@ ages6 = years.map((el,index)=> {
     age = now - el;
     return `Age element ${index + 1}: ${age}.`
 });
+*/
+
+
+
+
+
+
+
+
+/////////////////////////////////
+// CODING CHALLENGE
+
+/*
+
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
+
+class elem {
+    constructor(name, year){
+        this.id = new Date().valueOf();
+        this.name = name;
+        this.year = year;
+    }
+};
+
+class park extends elem{
+    constructor(name, area, nTrees, year){
+        super(id, name, year);
+        this. nTrees = nTrees;
+        this.area = area;
+    }
+}
