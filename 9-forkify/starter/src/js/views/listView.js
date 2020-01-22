6ed1e6b592b82bd869363146b77d1878a03bdd6f
Mattia Carducci <carducci@google.com>
2020-01-22 17:27:41 +0100

    ok

--- 9-forkify/starter/src/js/views/listView.js
+++ 9-forkify/starter/src/js/views/listView.js
@@ -4,7 +4,7 @@ export const renderItem = item =>{
     const markup = `
         <li class="shopping__item" data-itemid=${item.id}>
             <div class="shopping__count">
-                <input class="shopping_count-value" type="number" value="${item.count}" step="${item.count}">
+                <input class="shopping_count-value" type="number" value="${item.count}" step="${item.count}" min="0">
                 <p>${item.unit}</p>
             </div>
             <p class="shopping__description">${item.ingredient}</p>
